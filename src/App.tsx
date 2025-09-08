75b988dd18c4199609c099349301926916eca0d0
[dyad] <git@dyad.sh>
2025-09-08 12:04:09 -0500

    Reverted all changes back to version 2e68328d5947d975cee9fa3344355123798bdfad

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { ReservationPage } from "./pages/reserve/[businessId]";
 
 const queryClient = new QueryClient();
 
@@ -17,8 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* NEW ROUTE ADDED HERE */}
-          <Route path="/reserve/:businessId" element={<ReservationPage />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
