c87dbc7eab9b59c46b002263bd3aa9deff3af051
[dyad] <git@dyad.sh>
2025-09-11 11:03:23 -0500

    [dyad] Fixed routing error by adding BrowserRouter wrapper and restructuring App component - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
-import { AppHeader } from "@/components/AppHeader";
-import { ProtectedRoute } from "@/components/ProtectedRoute";
 import Index from "@/pages/Index";
 import Login from "@/pages/Login";
 import SignUp from "@/pages/SignUp";
@@ -9,39 +7,43 @@ import BusinessDashboard from "@/pages/business/Dashboard";
 import ConciergeDashboard from "@/pages/concierge/Dashboard";
 import NotFound from "@/pages/NotFound";
 import { AffiliateLink } from "@/pages/concierge/AffiliateLink";
+import { ProtectedRoute } from "@/components/ProtectedRoute";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route
-        path="/business/dashboard"
-        element={
-          <ProtectedRoute requiredRole="admin">
-            <BusinessDashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/concierge/dashboard"
-        element={
-          <ProtectedRoute requiredRole="concierge">
-            <ConciergeDashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/concierge/business/:businessId"
-        element={
-          <ProtectedRoute requiredRole="concierge">
-            <AffiliateLink />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/business/dashboard"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <BusinessDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/concierge/dashboard"
+          element={
+            <ProtectedRoute requiredRole="concierge">
+              <ConciergeDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/concierge/business/:businessId"
+          element={
+            <ProtectedRoute requiredRole="concierge">
+              <AffiliateLink />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Toaster />
+    </>
   );
 }
 
