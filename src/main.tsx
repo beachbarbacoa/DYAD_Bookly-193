bdbea99e75c85671fdc72367ab646fc1dd9b32ff
[dyad] <git@dyad.sh>
2025-09-15 10:30:54 -0500

    [dyad] Fixed default export consistency in App component - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";  // This should be a default import
-import "./globals.css";
+import { createRoot } from 'react-dom/client';
+import App from './App';  // This imports the default export
+import './globals.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(<App />);
